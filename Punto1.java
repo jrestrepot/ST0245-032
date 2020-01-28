3666a0a9c3f95ca4031ac7651e60ee9847a33faf
jrestrepot <60147041+jrestrepot@users.noreply.github.com>
2020-01-28 15:58:30 -0500

    Add files via upload
    Taller02

---
+++ Punto1.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Punto1 here.
+ *
+ * @author Juliana Restrepo y Andrea Carvajal
+ * @version (a version number or a date)
+ */
+public class Punto1
+//Se asume que p es mayor que q por indicaciones del profesor
+{
+    public int mcd(int p, int q){
+        if(p%q==0)
+        return q;
+        else 
+        return mcd(q,p%q);
+    }
+}
