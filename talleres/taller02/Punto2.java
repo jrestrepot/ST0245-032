a032832807d219ae07d232e4211d86e091fe8272
jrestrepot <60147041+jrestrepot@users.noreply.github.com>
2020-02-04 15:33:22 -0500

    Add files via upload

---
+++ talleres/taller02/Punto2.java
@@ -0,0 +1,41 @@
+
+
+/**
+
+ * Write a description of class Punto2 here.
+
+ *
+
+ * @author Juliana Restrepo y Andrea Carvajal
+
+ * @version (a version number or a date)
+
+ */
+
+public class Punto2
+
+{
+
+    public static boolean darkSouls(int i, int [] elemen, int max) {
+
+
+
+        if(i>=elemen.length){
+
+            return max==0;
+
+        }
+
+        else if (darkSouls(i+1,elemen,max-elemen[i]) || (darkSouls(i + 1, elemen, max))){
+
+            return true;
+
+        }
+
+        return false;
+
+
+
+    }
+
+}
