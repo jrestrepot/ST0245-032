e1ff2358e3e859d594c486a3dd9ee894d37fa7fa
jrestrepot <60147041+jrestrepot@users.noreply.github.com>
2020-02-29 17:55:06 -0500

    Add files via upload

---
+++ laboratorios/lab02/codigo/Punto1.py
@@ -0,0 +1,58 @@
+import random
+import time
+def generate(n):
+    arr=[]
+    for b in range(n):
+        arr.append(random.randint(0,10000))
+    return arr
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Finding the mid of the array
+        L = arr[:mid]  # Dividing the array elements
+        R = arr[mid:]  # into 2 halves
+
+        mergeSort(L)  # Sorting the first half
+        mergeSort(R)  # Sorting the second half
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+            while i < len(L):
+                arr[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(R):
+                arr[k] = R[j]
+                j += 1
+                k += 1
+
+
+def insertionSort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+for a in range(200000,240000,2000):
+    c=time.time()
+    mergeSort(generate(a))
+    print(time.time()-c)
